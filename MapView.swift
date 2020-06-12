c453b46052177c7cc0b8cd49d0566f2f7b2c2654
Chris Wu <31398736+shiftingsand@users.noreply.github.com>
2020-06-12 14:41:34 -0400

    Day 69 - "Communicating with a MapKit coordinator"

--- MapView.swift
+++ MapView.swift
@@ -10,8 +10,37 @@ import SwiftUI
 import MapKit
 
 struct MapView : UIViewRepresentable {
+    class Coordinator : NSObject, MKMapViewDelegate {
+        var parent : MapView
+        
+        init(_ parent : MapView) {
+            self.parent = parent
+        }
+        
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            print(mapView.centerCoordinate)
+        }
+        
+        func mapView(_ mapview : MKMapView, viewFor annotation : MKAnnotation) -> MKAnnotationView? {
+            let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+            view.canShowCallout = true
+            return view
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = "Tokyo DisneySea"
+        annotation.coordinate = CLLocationCoordinate2DMake(35.6267, 139.8851)
+        mapView.addAnnotation(annotation)
+        
         return mapView
     }
     
