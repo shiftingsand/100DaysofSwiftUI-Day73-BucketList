ddf2f7bd1011cceb965d291900a0a56957e872fc
Chris Wu <31398736+shiftingsand@users.noreply.github.com>
2020-06-16 14:32:24 -0400

    Challenge 2 done.

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -11,46 +11,12 @@ import LocalAuthentication
 import MapKit
 
 struct ContentView: View {
-    let myFilename = "SavedPlaces"
-    @State var centerCoordinate = CLLocationCoordinate2D()
-    @State var locations = [CodableMKPointAnnotation]()
-    @State var selectedPlace: MKPointAnnotation?
-    @State private var showingPlaceDetails = false
-    @State private var showingEditScreen = false
     @State private var isUnlocked = true
     
     var body: some View {
         ZStack {
             if isUnlocked {
-                Mappy(centerCoordinate: $centerCoordinate, locations: locations, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails)
-                Circle()
-                    .fill(Color.blue)
-                    .opacity(0.3)
-                    .frame(width: 32, height: 32)
-                
-                VStack {
-                    Spacer()
-                    HStack {
-                        Spacer()
-                        Button(action: {
-                            let newLocation = CodableMKPointAnnotation()
-                            newLocation.title = "Example Location"
-                            newLocation.coordinate = self.centerCoordinate
-                            self.locations.append(newLocation)
-                            self.selectedPlace = newLocation
-                            self.showingEditScreen = true
-                        }
-                        ) {
-                            Image(systemName: "plus")
-                                .padding()
-                                .background(Color.black.opacity(0.75))
-                                .foregroundColor(.white)
-                                .font(.title)
-                                .clipShape(Circle())
-                                .padding(.trailing)
-                        }
-                    }
-                }
+                Mappy()
             } else {
                 Button("Unlock places") {
                     self.authenticate()
@@ -61,43 +27,6 @@ struct ContentView: View {
                 .clipShape(Capsule())
             }
         } // zstack
-        .onAppear(perform: loadData)
-        .alert(isPresented: $showingPlaceDetails){
-            Alert(title: Text(selectedPlace?.title ?? "Unknown"), message: Text(selectedPlace?.subtitle ?? "Missing place information."), primaryButton: .default(Text("OK")), secondaryButton: .default(Text("Edit")) {
-                self.showingEditScreen = true
-            })
-        }
-        .sheet(isPresented: $showingEditScreen, onDismiss: saveData) {
-            if self.selectedPlace != nil {
-                EditView(placemark: self.selectedPlace!)
-            }
-        }
-    }
-    
-    func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
-    }
-    
-    func loadData() {
-        let filename = getDocumentsDirectory().appendingPathComponent(myFilename)
-        
-        do {
-            let data = try Data(contentsOf: filename)
-            locations = try JSONDecoder().decode([CodableMKPointAnnotation].self, from: data)
-        } catch {
-            print("Unable to load saved data!")
-        }
-    }
-    
-    func saveData() {
-        do {
-            let filename = getDocumentsDirectory().appendingPathComponent(myFilename)
-            let data = try JSONEncoder().encode(self.locations)
-            try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
-        } catch {
-            print("Unable to save data!")
-        }
     }
     
     func authenticate() {
